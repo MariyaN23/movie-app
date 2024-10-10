9a3cda90e8e2bd04cdcb8dbcf6a5adb37540618f
MariyaN23 <sellaite505@gmail.com>
2024-10-10 15:41:28 +0300

    input search

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import {NavBarComponent} from '../../components/nav-bar/nav-bar.component';
+import {SegmentedControlComponent} from '../../components/segmented-control/segmented-control.component';
+import {SegmentedControlConfig} from '../../interfaces/ui-configs/segmented-control-config.interface';
+import {InputComponent} from '../../components/input/input.component';
 
 @Component({
   selector: 'app-test',
   standalone: true,
   imports: [
-    NavBarComponent
+    NavBarComponent, SegmentedControlComponent, InputComponent
   ],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
 export class TestComponent {
-
+  segments: SegmentedControlConfig[] = [
+    {
+      name: 'All',
+      active: true
+    },
+    {
+      name: 'Movies',
+      active: false
+    },
+    {
+      name: 'TV Shows',
+      active: false
+    },
+  ]
 }
